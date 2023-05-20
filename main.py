32006601517a852ad37a634a2714ea248f4894d7
teawon <xodnjsvkvk9@gmail.com>
2023-05-20 14:57:04 +0900

    chore : 코드 분석 및 주석

--- main.py
+++ main.py
@@ -31,11 +31,23 @@ if __name__ == "__main__":
             # Read feed
             ret, frame = cap.read()
 
-            # Make detections
+            # 0. 결과 값을 return할 수 있는 String으로 추출해보기
+
+            # 1. 일단 더미데이터를 넣어서 결과값이 추출가능한지 수정해보기
+            
+            # 2. record모드와 상관없이 항상 데이터를 뽑아서 보내지는지 확인해보기 (어처피 프론트에서 손동작을 안하면 데이터를 안보낼 것)
+            
+            # 3. 프론트에서 보내고 있는 배열값을 해당 함수에서 사용하고있는 방식으로 파싱할 것
+
+            # 4. 웹소켓 연결 및 실제 api연동해보기
+
+            # 여기서 캠에서 읽어온 이미지와 라벨정보를 props로 넘긴다
             image, results = mediapipe_detection(frame, holistic)
 
+            
             # Process results
             sign_detected, is_recording = sign_recorder.process_results(results)
+
 
             # Update the frame (draw landmarks & display result)
             webcam_manager.update(frame, results, sign_detected, is_recording)
--- sign_recorder.py
+++ sign_recorder.py
@@ -8,8 +8,8 @@ from utils.landmark_utils import extract_landmarks
 
 
 class SignRecorder(object):
-    def __init__(self, reference_signs: pd.DataFrame, seq_len=50):
-        # Variables for recording
+    def __init__(self, reference_signs: pd.DataFrame, seq_len=100):
+        # 초기 모드 값 설정 (데이터를 모으는 길이, recording여부 설정)
         self.is_recording = False
         self.seq_len = seq_len
 
@@ -20,26 +20,22 @@ class SignRecorder(object):
         self.reference_signs = reference_signs
 
     def record(self):
-        """
-        Initialize sign_distances & start recording
-        """
+        # recording시작 - 시작 시점부터 데이터를 모은다
+
         self.reference_signs["distance"].values[:] = 0
         self.is_recording = True
 
     def process_results(self, results) -> (str, bool):
-        """
-        If the SignRecorder is in the recording state:
-            it stores the landmarks during seq_len frames and then computes the sign distances
-        :param results: mediapipe output
-        :return: Return the word predicted (blank text if there is no distances)
-                & the recording state
-        """
+       
+
         if self.is_recording:
+            # 1. 데이터가 덜 쌓였다면 랜드마크값을 더 추가
+    
             if len(self.recorded_results) < self.seq_len:
                 self.recorded_results.append(results)
             else:
+            # 2. 데이터가 충분하다면 계산 시작
                 self.compute_distances()
-                print(self.reference_signs)
 
         if np.sum(self.reference_signs["distance"].values) == 0:
             return "", self.is_recording
@@ -55,6 +51,11 @@ class SignRecorder(object):
             _, left_hand, right_hand = extract_landmarks(results)
             left_hand_list.append(left_hand)
             right_hand_list.append(right_hand)
+
+        print("왼쪽 좌표 추출")
+        print(left_hand_list)
+        print("오른쪽 좌표 추출")
+        print(right_hand_list)
 
         # Create a SignModel object with the landmarks gathered during recording
         recorded_sign = SignModel(left_hand_list, right_hand_list)
@@ -80,11 +81,16 @@ class SignRecorder(object):
         """
         # Get the list (of size batch_size) of the most similar reference signs
         sign_names = self.reference_signs.iloc[:batch_size]["name"].values
+        print("sign_name")
+        print(sign_names)
 
         # Count the occurrences of each sign and sort them by descending order
         sign_counter = Counter(sign_names).most_common()
+        print("sign_counter")
+        print(sign_counter)
+
 
         predicted_sign, count = sign_counter[0]
-        if count / batch_size < threshold:
-            return "Signe inconnu"
+        # if count / batch_size < threshold:
+        #     return "Signe inconnu"
         return predicted_sign
